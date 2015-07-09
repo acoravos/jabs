81c5d1f55d53d6859a534949c15af35d0bc22095
Andrea Coravos & Ian & Sung Bin Choi <pair+acoravos.raidersofthenight.sungry@devbootcamp.com>
2015-07-09 11:34:00 -0700

    Add controllers and views for signup and login

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,38 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/user' do
+
+  erb :users
+end
+
+get '/user/:id' do
+
+  erb :profile
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:username])
+
+  if @user.password == params[:password]
+    session[:current_user_id] = @user.id
+    session[:current_username] = @user.username
+    redirect '/'
+    #else wrong password will redirect to a separate page
+  end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @user = User.new(username: params[:username])
+  @user.password = params[:password]
+  @user.save!
+  session[:current_user_id] = @user.id
+  session[:current_username] = @user.username
+  redirect '/'
+end
+
