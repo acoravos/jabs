e6bc6be41b5607dbd2e7c628485392909f6250a4
John Hess & Bikram Virk <pair+alookatommorow.bvirk1357@devbootcamp.com>
2015-07-09 12:41:41 -0700

    Added Followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   # users.password_hash in the database is a :string
   include BCrypt
 
+
+  has_many :leaders, foreign_key: "follower_id", class_name: "Following"
+  has_many :leads, through: :leaders
+
+  has_many :followers, foreign_key: "leader_id", class_name: "Following"
+  has_many :follows, through: :followers
+
+
+
+
   def password
     @password ||= Password.new(password_hash)
   end
@@ -12,4 +22,7 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_hash = @password
   end
+
+
+
 end
